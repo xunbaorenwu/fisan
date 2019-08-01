7a38b591122c8279d618d5e8c047005583ce2fcc
zhangchuan <quan-147123@163.com>
2019-08-01 22:15:11 +0800

    haha

--- haha/Application/Common/Conf/config.php
+++ haha/Application/Common/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '192.168.153.128', // 服务器地址
+    'DB_HOST'               =>  '192.168.1.101', // 服务器地址
     'DB_NAME'               =>  'test',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'root',          // 密码
